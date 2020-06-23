86e4e1356bff61eb1445868df0f4bfe9bc4c78ac
Andres Reyes <karlosareyes@gmail.com>
2020-06-23 15:55:42 -0500

    Primer actualización a footer.

---
+++ js/carrousel.js
@@ -0,0 +1,11 @@
+let btPrev = document.getElementById('btPrevCar');
+let btNext = document.getElementById('btNextCar');
+
+
+btPrev.addEventListener('click', ()=>{
+	alert("Atras <===");
+})
+
+btNext.addEventListener('click', ()=>{
+	alert("Adelante ===>");
+})
